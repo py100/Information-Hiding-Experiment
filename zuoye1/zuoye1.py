c35d2e95228e1b753342ad877747404f86a1ceaf
JevonsAn <anyu.hao@163.com>
2018-03-16 20:28:47 +0800

    change the output

--- zuoye1/zuoye1.py
+++ zuoye1/zuoye1.py
@@ -3,25 +3,6 @@ from math import cos, pi
 import sys
 
 
-def DCT(temp8):
-    S = []
-    for v in range(8):
-        heng = []
-        for u in range(8):
-            Cu = Cv = 1
-            if ~v:
-                Cv = 2**(-0.5)
-            if ~u:
-                Cu = 2**(-0.5)
-            t = 0
-            for y in range(8):
-                for x in range(8):
-                    t += temp8[x][y] * cos((2 * x + 1) * v * pi / 16) * cos((2 * y + 1) * u * pi / 16)
-            heng.append(0.25 * Cu * Cv * t)
-        S.append(heng)
-    return S
-
-
 def YinXie(origin, info):
     if origin % 2 == 0:
         if info == 0:
@@ -33,14 +14,6 @@ def YinXie(origin, info):
             return origin
         elif info == 0:
             return origin - 1
-
-
-def my_print(temp, result_file):
-    for i in range(8):
-        for j in range(8):
-            result_file.write(str(temp[i][j]) + " ")
-        result_file.write("\n")
-    result_file.write("\n")
 
 
 LHTable = [[16, 11, 10, 16, 24, 40, 51, 61],
@@ -71,25 +44,42 @@ def information_hide(picture, secret, result_filename):
     width = im.size[0]
     height = im.size[1]
     source = im.copy().load()
-    for i in range(8, width - width % 8 - 8, 8):
-        for j in range(0, height - height % 8 - 8, 8):
-            temp = []
-            for x in range(8):
-                heng = []
-                for y in range(8):
-                    heng.append(source[i + x, j + y])
-                temp.append(tuple(heng))
-            temp = DCT(temp)
-            for x in range(8):
-                for y in range(8):
-                    temp[x][y] = round(temp[x][y] / LHTable[x][y])
+    new_width = width - width % 8
+    new_height = height - height % 8
+
+    new_Source = [[0 * x * i for x in range(new_height)] for i in range(new_width)]
+    result_file.write("%d %d\n" % (new_width, new_height))
+    print(len(new_Source), len(new_Source[0]))
+
+    for i in range(8, new_width - 8, 8):
+        for j in range(0, new_height - 8, 8):
+
+            for v in range(8):
+                for u in range(8):
+                    Cu = Cv = 1
+                    if not v:
+                        Cv = 2**(-0.5)
+                    if not u:
+                        Cu = 2**(-0.5)
+                    t = 0
+                    for y in range(8):
+                        for x in range(8):
+                            t += source[i + y, j + x] * cos((2 * x + 1) * u * pi / 16) * cos((2 * y + 1) * v * pi / 16)
+                    t = 0.25 * Cu * Cv * t
+
+                    t = round(t / LHTable[v][u])
                     if not stop_flag:
-                        if temp[x][y] not in (0, 1, -1):
-                            temp[x][y] = YinXie(temp[x][y], Bsecret[secret_index])
+                        if t not in {0, 1, -1}:
+                            t = YinXie(t, Bsecret[secret_index])
                             secret_index += 1
                             if secret_index == secret_len:
                                 stop_flag = True
-            my_print(temp, result_file)
+                    new_Source[i + v][j + u] = t
+
+    for i in range(new_height):
+        for j in range(new_width):
+            result_file.write("%d " % new_Source[j][i])
+        result_file.write("\n")
 
     result_file.close()
 
@@ -97,7 +87,10 @@ def information_hide(picture, secret, result_filename):
 if __name__ == "__main__":
     secret = "I love you!"
     if len(sys.argv) == 1:
-        print("未指定信息载体图片。")
+        picture = "small.bmp"
+        result_filename = "result.txt"
+        information_hide(picture, secret, result_filename)
+        # print("未指定信息载体图片。")
     elif len(sys.argv) == 2:
         picture = sys.argv[1]
         result_filename = "result.txt"
